24009622be6d4e259f0ba8bce3b2d42144697406
Kirino Minato <kirino.minato@gmail.com>
2017-03-23 15:41:32 +0900

    add security groups

--- Terraform/modules/vpc-subnet/main.tf
+++ Terraform/modules/vpc-subnet/main.tf
@@ -35,6 +35,316 @@ resource "aws_network_acl" "acl" {
   }
 }
 
+resource "aws_default_security_group" "vpc_default_sg" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  # Inbound Rules (AWS Default Policy)
+  ingress {
+    protocol  = -1
+    self      = "true"
+    from_port = 0
+    to_port   = 0
+  }
+
+  # Outbound Rules (AWS Default Policy)
+  egress {
+    protocol   = -1
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 0
+    to_port    = 0
+  }
+}
+
+resource "aws_security_group" "sg_linux_default" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  name        = "sg-linux-default"
+  description = "sg-linux-default"
+
+  tag {
+    Name = "sg-linux-default"
+  }
+
+  # Inbound ICMP (ping, traceroute, etc / only private networks)
+  ingress {
+    protocol   = "icmp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 0
+    to_port    = 0
+  }
+
+  # Inbound SSH
+  ingress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", "0.0.0.0/0"]
+    from_port  = 22
+    to_port    = 22
+  }
+
+  # Outbound ICMP (ping, traceroute, etc)
+  egress {
+    protocol   = "icmp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 0
+    to_port    = 0
+  }
+
+  # Outbound SSH
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 22
+    to_port    = 22
+  }
+
+  # Outbound DNS (udp)
+  egress {
+    protocol   = "udp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 53
+    to_port    = 53
+  }
+
+  # Outbound DNS (tcp)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 53
+    to_port    = 53
+  }
+
+  # Outbound NTP
+  egress {
+    protocol   = "udp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 123
+    to_port    = 123
+  }
+
+  # Outbound HTTP
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # Outbound HTTPS
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 443
+    to_port    = 443
+  }
+}
+
+resource "aws_security_group" "sg_windows_default" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  name        = "sg-windows-default"
+  description = "sg-windows-default"
+
+  tag {
+    Name = "sg-windows-default"
+  }
+
+  # Inbound ICMP (ping, traceroute, etc / only private networks)
+  ingress {
+    protocol   = "icmp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 0
+    to_port    = 0
+  }
+
+  # Inbound WinRM (5985=Over HTTP, 5986=Over HTTPS / only private networks)
+  ingress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 5985
+    to_port    = 5986
+  }
+
+  # Inbound RDP
+  ingress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", "0.0.0.0/0"]
+    from_port  = 3389
+    to_port    = 3389
+  }
+
+  # Outbound ICMP (ping, traceroute, etc)
+  egress {
+    protocol   = "icmp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 0
+    to_port    = 0
+  }
+
+  # Outbound Ports
+  # Active Directory and Active Directory Domain Services Port Requirements
+  # ref: https://technet.microsoft.com/en-us/library/8daead2d-35c1-4b58-b123-d32a26b1f1dd
+
+  # Outbound DNS (udp)
+  egress {
+    protocol   = "udp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 53
+    to_port    = 53
+  }
+  # Outbound DNS (tcp)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 53
+    to_port    = 53
+  }
+  # Outbound NTP
+  egress {
+    protocol   = "udp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 123
+    to_port    = 123
+  }
+  # Outbound HTTP
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 80
+    to_port    = 80
+  }
+  # Outbound HTTPS
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 443
+    to_port    = 443
+  }
+  # Outbound HTTPS
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+    from_port  = 443
+    to_port    = 443
+  }
+  # Outbound LDAP (tcp)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 389
+    to_port    = 389
+  }
+  # Outbound LDAP (udp)
+  egress {
+    protocol   = "udp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 389
+    to_port    = 389
+  }
+  # Outbound LDAP SSL
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 636
+    to_port    = 636
+  }
+  # Outbound LDAP GC / LDAP GC SSL
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 3268
+    to_port    = 3269
+  }
+  # Outbound Kerberos (tcp)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 88
+    to_port    = 88
+  }
+  # Outbound Kerberos (udp)
+  egress {
+    protocol   = "udp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 88
+    to_port    = 88
+  }
+  # Outbound SMB,CIFS,DFSN,LSARPC,NbtSS,NetLogonR,SamR,SrvSvc (tcp)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 445
+    to_port    = 445
+  }
+  # Outbound SMB,CIFS,DFSN,LSARPC,NbtSS,NetLogonR,SamR,SrvSvc (udp)
+  egress {
+    protocol   = "udp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 445
+    to_port    = 445
+  }
+  # Outbound SMTP(Replication)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 25
+    to_port    = 25
+  }
+  # Outbound TCP dynamic ports (1024-65535)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 1024
+    to_port    = 65535
+  }
+  # Outbound Kerberos change/set password (tcp)
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 464
+    to_port    = 464
+  }
+  # Outbound Kerberos change/set password (udp)
+  egress {
+    protocol   = "udp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 464
+    to_port    = 464
+  }
+  # Outbound UDP dynamic ports (1024-65535)
+  egress {
+    protocol   = "udp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 1024
+    to_port    = 65535
+  }
+  # Outbound DFS, Group Policy
+  egress {
+    protocol   = "udp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 138
+    to_port    = 138
+  }
+  # Outbound SOAP
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 9389
+    to_port    = 9389
+  }
+  # Outbound NetLogon,NetBIOS,Name Resolution
+  egress {
+    protocol   = "udp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 137
+    to_port    = 137
+  }
+  # Outbound DFSN,NetBIOS Session Service,NetLogon
+  egress {
+    protocol   = "tcp"
+    cidr_block = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    from_port  = 139
+    to_port    = 139
+  }
+}
+
 resource "aws_subnet" "subnet" {
   vpc_id                          = "${aws_vpc.vpc.id}"
   count                           = "${length(var.vpc_subnet_cidr_list)}"
