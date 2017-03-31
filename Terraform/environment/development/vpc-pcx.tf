75e8e7267910004ccdc7c51eac042a638e114a68
imksoo <kirino.minato@gmail.com>
2017-03-31 14:10:20 +0900

    add vpc peering

--- Terraform/environment/development/vpc-pcx.tf
+++ Terraform/environment/development/vpc-pcx.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "piano-infra-dev01" {
-  vpc_id        = "${vpc.vpc_id}"
+  vpc_id        = "${module.vpc.vpc_id}"
   peer_owner_id = "528598000004"
   peer_vpc_id   = "vpc-2a34604d"
   auto_accept   = false
